60594b00efb86fdcdc9950e09ef27cda22497fff
Hasan Ozovali <ozovalihasan@gmail.com>
2020-11-04 14:39:50 +0300

    Add operate function

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,1 +1,30 @@
-operate.js;
+import Big from 'big.js';
+
+const operate = (numberOne, numberTwo, operation) => {
+  let result;
+  const numberOneBig = Big(numberOne);
+  const numberTwoBig = Big(numberTwo);
+  switch (operation) {
+    case '-':
+      result = numberOneBig - numberTwoBig;
+      break;
+    case '+':
+      result = numberOneBig + numberTwoBig;
+      break;
+    case '/':
+      result = numberOneBig / numberTwoBig;
+      break;
+    case 'X':
+      result = numberOneBig * numberTwoBig;
+      break;
+    case '%':
+      result = numberOneBig / 100;
+      break;
+    default:
+      break;
+  }
+
+  return result;
+};
+
+export default operate;
