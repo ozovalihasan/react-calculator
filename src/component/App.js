7bb7ca2b9ac18c3ca7cc6126dc109dcdc8bf2fdf
Hasan Ozovali <ozovalihasan@gmail.com>
2020-11-04 17:32:23 +0300

    Fix errors shown by react

--- src/component/App.js
+++ src/component/App.js
@@ -2,11 +2,10 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import '../index.scss';
-import calculate from '../logic/calculate';
+// import calculate from '../logic/calculate';
 
 const App = () => (
   <div className="app">
-    {console.log(calculate({ total: '3', next: '2', operation: '+' }, '+'))}
     <Display />
     <ButtonPanel />
   </div>
