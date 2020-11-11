9b3a579cf970d1286ac9e023834b520b7046f8c8
Hasan Ozovali <ozovalihasan@gmail.com>
2020-11-11 11:48:13 +0300

    Update hooks with correct one

--- src/component/App.js
+++ src/component/App.js
@@ -1,13 +1,13 @@
-import React, { setState } from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 import '../index.scss';
 
 const App = () => {
-  const [total, setTotal] = setState('');
-  const [next, setNext] = setState('');
-  const [operation, setOperation] = setState('');
+  const [total, setTotal] = useState('');
+  const [next, setNext] = useState('');
+  const [operation, setOperation] = useState('');
 
   const handleClick = buttonName => {
     const result = calculate({ next, total, operation }, buttonName);
