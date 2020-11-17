f846e19f860599881bf99f07864a778e957183bf
Hasan Ozovali <ozovalihasan@gmail.com>
2020-11-17 10:35:45 +0300

    Update navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 const Navbar = () => (
   <div className="navbar main">
     <ul className="navbar container">
-      <h2 className="navbar title"> Math Magicians</h2>
+      <h2 className="navbar title">Math Magicians</h2>
       <li>
-        <Link to="/">Home |</Link>
+        <Link to="/">Home</Link>
       </li>
+      |
       <li>
-        <Link to="/calculator">Calculator |</Link>
+        <Link to="/calculator">Calculator</Link>
       </li>
+      |
       <li>
         <Link to="/quote">Quote</Link>
       </li>
